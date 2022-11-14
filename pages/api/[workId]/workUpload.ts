9ca323997bff3398c745d74d5a0ae0417be77818
stew.bracken <stew.bracken@gmail.com>
2022-11-13 18:51:21 -0800

    log work upload

--- pages/api/[workId]/workUpload.ts
+++ pages/api/[workId]/workUpload.ts
@@ -21,99 +21,110 @@ export default async function handler(
     return;
   }
 
-  await initializeIfNeeded();
-
-  const ctx = await getContext(req);
-  if (!ctx.authorized || !ctx.user) {
-    res.status(401).json({ message: "unauthorized" });
-    return;
-  }
-
-  const { workId } = req.query;
-
-  if (typeof workId !== "string" || !Number.isFinite(parseInt(workId))) {
-    res.status(400).json({ message: "bad id" });
-    return;
-  }
-
-  const work = await stores().project.getProject(workId);
-
-  if (work?.owner.id !== ctx.user.id) {
-    res.status(404).json({ message: "not found" });
-    return;
-  }
-
-  const form = new multiparty.Form({ maxFilesSize: 1000000, autoFiles: true });
-  let data: any;
   try {
-    data = await new Promise<any>((resolve, reject) => {
-      form.parse(req, function (err, fields, files) {
-        if (err) reject({ err });
-        resolve({ fields, files });
+    await initializeIfNeeded();
+
+    const ctx = await getContext(req);
+    if (!ctx.authorized || !ctx.user) {
+      res.status(401).json({ message: "unauthorized" });
+      return;
+    }
+
+    const { workId } = req.query;
+
+    if (typeof workId !== "string" || !Number.isFinite(parseInt(workId))) {
+      res.status(400).json({ message: "bad id" });
+      return;
+    }
+
+    const work = await stores().project.getProject(workId);
+
+    if (work?.owner.id !== ctx.user.id) {
+      res.status(404).json({ message: "not found" });
+      return;
+    }
+
+    const form = new multiparty.Form({
+      maxFilesSize: 1000000,
+      autoFiles: true,
+    });
+    let data: any;
+    try {
+      data = await new Promise<any>((resolve, reject) => {
+        form.parse(req, function (err, fields, files) {
+          if (err) reject({ err });
+          resolve({ fields, files });
+        });
       });
-    });
-    console.log(`data: `, JSON.stringify(data));
-    if (Object.keys(data.files).length !== 1) {
-      res.status(400).json({ name: "too many 'file'" });
-      return;
-    }
-    const file = data.files["file"];
-    if (!file) {
-      res.status(400).json({ name: "missing 'file'" });
-      return;
-    }
-    if (file.length !== 1) {
-      res.status(400).json({ name: "multiple 'file' entries" });
-      return;
-    }
-    const fileObj = file[0];
-
-    if (fileObj.headers["content-type"] !== "application/zip") {
-      res.status(400).json({ name: "not application/zip" });
-      return;
-    }
-    /// delete the prior work from ipfs
-    if (work.codeCid) {
-      const existinWorkId = await getMetadataWorkId(work.codeCid);
-      if (existinWorkId === work.id) {
-        //only delete it if the current user owns it.
-        await deleteCid(work.codeCid);
+      console.log(`data: `, JSON.stringify(data));
+      if (Object.keys(data.files).length !== 1) {
+        res.status(400).json({ name: "too many 'file'" });
+        return;
       }
-    }
-    // const project = stores().project.getProject(workId);
-    //upload to pinata and set the url.
-    const tmpPath = fileObj.path;
-    const cid = await pinZipToPinata(tmpPath, { workId: work.id });
-    if (!cid) {
-      res.status(400).json({ message: "cid missing" });
-      return;
-    }
+      const file = data.files["file"];
+      if (!file) {
+        res.status(400).json({ name: "missing 'file'" });
+        return;
+      }
+      if (file.length !== 1) {
+        res.status(400).json({ name: "multiple 'file' entries" });
+        return;
+      }
+      const fileObj = file[0];
 
-    const updateRes = await stores().project.updateProject(ctx?.user, {
-      codeCid: cid,
-      id: work.id,
-    });
-    if (!updateRes.ok) {
-      return res.status(500).json({ message: "failed to upload" });
-    }
+      if (fileObj.headers["content-type"] !== "application/zip") {
+        res.status(400).json({ name: "not application/zip" });
+        return;
+      }
+      /// delete the prior work from ipfs
+      if (work.codeCid) {
+        const existinWorkId = await getMetadataWorkId(work.codeCid);
+        if (existinWorkId === work.id) {
+          //only delete it if the current user owns it.
+          await deleteCid(work.codeCid);
+        }
+      }
+      // const project = stores().project.getProject(workId);
+      //upload to pinata and set the url.
+      const tmpPath = fileObj.path;
+      const cid = await pinZipToPinata(tmpPath, { workId: work.id });
+      if (!cid) {
+        res.status(400).json({ message: "cid missing" });
+        return;
+      }
 
-    res.status(200).json({ url: ipfsUrl(cid) });
-  } finally {
-    if (data?.files) {
-      for (const fileName of Object.keys(data.files)) {
-        const fileArr = data.files[fileName];
-        for (const file of fileArr) {
-          try {
-            fs.rmSync(file.path, {
-              force: true,
-            });
-            console.log("deleted", file.path);
-          } catch (e) {
-            console.error("failed to delete");
+      const updateRes = await stores().project.updateProject(ctx?.user, {
+        codeCid: cid,
+        id: work.id,
+      });
+      if (!updateRes.ok) {
+        return res.status(500).json({ message: "failed to upload" });
+      }
+
+      return res.status(200).json({ url: ipfsUrl(cid) });
+    } finally {
+      if (data?.files) {
+        for (const fileName of Object.keys(data.files)) {
+          const fileArr = data.files[fileName];
+          for (const file of fileArr) {
+            try {
+              fs.rmSync(file.path, {
+                force: true,
+              });
+              console.log("deleted", file.path);
+            } catch (e) {
+              console.error("failed to delete");
+            }
           }
         }
       }
     }
+  } catch (e) {
+    console.error("Error uploading work: ", e);
+    res.status(500).json({
+      message:
+        "something went wrong uploading your work." + (e as any)?.message,
+    });
   }
 }
 export const config = {
