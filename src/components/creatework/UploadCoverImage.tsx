ca41ab9c77e3bcbe2629396897422146052ace29
Stewart <stew.bracken@gmail.com>
2023-01-30 19:48:54 -0800

    New image cover

--- src/components/creatework/UploadCoverImage.tsx
+++ src/components/creatework/UploadCoverImage.tsx
@@ -66,7 +66,9 @@ export const UploadCoverImage: FC<CreateWorkProps> = (
       <h2>Collection Image</h2>
       <FlexBoxCenter fluid={false}>
         <div>
-          <DropZone accept={"images"} onUpload={onUpload}></DropZone>
+          <DropZone accept={"images"} onUpload={onUpload}>
+            Drag 'n' drop your image here, or click to select a file
+          </DropZone>
           {mutation.isLoading && <SpinnerLoading></SpinnerLoading>}
           {mutation.isSuccess && "Success!"}
         </div>
