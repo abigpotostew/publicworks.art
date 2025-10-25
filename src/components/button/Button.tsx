9d9d6ac02af08e8ac519966818f549e144103e5e
Stewart <stew.bracken@gmail.com>
2025-10-25 11:56:33 -0700

    ok

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,9 @@
 // @flow
-import React, { FC, ForwardedRef } from "react";
+import React, { FC, ForwardedRef, ElementType } from "react";
 import { ButtonProps } from "react-bootstrap/Button";
 import { Button } from "react-bootstrap";
 
-type Props = ButtonProps;
+type Props = ButtonProps & { as?: ElementType };
 export const ButtonPW: FC<Props> = (props: Props) => {
   return <Button variant={props.variant || "primary"} {...props}></Button>;
 };
